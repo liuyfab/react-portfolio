76519646cd648f0ad6bb4a85e0fe468e01a85b50
liuyfab <liuyongfeng83@gmail.com>
2022-02-12 16:02:36 -0600

    add pages files

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,4 +1,6 @@
 import React from 'react'; 
+import resume from "../../assets/pdf/yongfeng_CV.docx";
+
 
 function Resume(){
     return(<div>
@@ -6,6 +8,26 @@ function Resume(){
         <hr/>
         <ul>
             Skills Achieved: 
+        <h3 className="list-header">Front-end skills</h3>   
+            <ul className="prof-list">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>responsive design</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+        </ul>
+
+        <h3 className="list-header">Back-end skills</h3>
+        <ul className="prof-list">
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST</li>
+        </ul>
         </ul>
     </div>)
 }
